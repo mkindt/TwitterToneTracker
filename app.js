35b34162b36b73c99e8222d27a6436981e18a5db
mkindt <mkindt@unca.edu>
2013-04-11 02:02:52 -0400

    cleaned up code, fixed tabbing

--- app.js
+++ app.js
@@ -1,91 +1,66 @@
 
 var express = require("express")
-  , routes = require("./routes")
-  , user = require("./routes/user")
-  , http = require("http")
-  , path = require("path")
-  , redisClient = require("redis").createClient()
-  , app = express()
-  , twitterWorker = require("./twitter.js")
-  , trackedWords = ["good", "bad", "yeah", "no", "could", "should", "thanks", "sorry"];
-  // symmetry - for each happy word, please follow with opposite sad word
+    , routes = require("./routes")
+    , user = require("./routes/user")
+    , http = require("http")
+    , path = require("path")
+    , redisClient = require("redis").createClient()
+    , app = express()
+    , twitterWorker = require("./twitter.js")
+    , trackedWords = ["good", "bad", "yeah", "no", "could", "should", "thanks", "sorry"];
+    // symmetry - for each happy word, please follow with opposite sad word
+    
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-//app.get('/', routes.index);
-//app.get('/users', user.list);
-app.get("/", function (req, res) {
-  var response = "hello there";
-  //res.send("hi");
-  res.send(response);
-});
-
-app.get("/goodbye", function (req, res) {
-  //send "Goodbye World" to the client as html
-  res.send("Goodbye World!");
-});
-
-app.get("/login", function (req, res) {
-  res.send("You need to login!");
+    app.use(express.errorHandler());
 });
 
 app.get("/counts.json", function	(req, res) {
     var happyCount = 0;
     var sadCount = 0;
     redisClient.mget(trackedWords, function	(error, results) {
-      var jsonObject = [];
-      var i;
-	    if (error !== null) {
-            // handle error here                                                                                                                       
-            console.log("ERROR: " + error);
-        } else {
-          for (i = 0; i < trackedWords.length; i = i + 1) {
-            if (i%2 === 0){
-                jsonObject.push( {
-                    "happyKey": trackedWords[i],
-		                "count":results[i],
-                });
-                happyCount = happyCount + parseInt(results[i], 10);
+        var jsonObject = [];
+        var i;
+	      if (error !== null) {
+              // handle error here                                                                                                                       
+              console.log("ERROR: " + error);
+        }
+        else {
+            for (i = 0; i < trackedWords.length; i = i + 1) {
+                if (i%2 === 0){
+                    jsonObject.push( {
+                        "happyKey": trackedWords[i],
+    		                "count":results[i],
+                    });
+                    happyCount = happyCount + parseInt(results[i], 10);
+                }
+                else {
+                    jsonObject.push( {
+                        "sadKey": trackedWords[i],
+    		                "count":results[i],
+                    });
+                    sadCount = sadCount + parseInt(results[i], 10);
+                }
             }
-            else {
-                jsonObject.push( {
-                    "sadKey": trackedWords[i],
-		                "count":results[i],
-                });
-                sadCount = sadCount + parseInt(results[i], 10);
-            }
-          }
-          jsonObject.push( {"happyCount":happyCount, "sadCount":sadCount} );
-            // use res.json to return JSON objects instead of strings
+            jsonObject.push( {"happyCount":happyCount, "sadCount":sadCount} );
             res.json(jsonObject);
         }
     });
 });
-// app.get("todos.json", function (req,res) {
-// client.mget....
-// var response = "{'key':'value'}";
-// res.writehead(200, {"Content-Type": "application/json"});
-//////////////////////////////////////////
-// app.get("counts.json", function (req,res) {
-  // redisClient.mget... 
-// client.mget....
-// var response = "{'key':'value'}";
-// res.writehead(200, {"Content-Type": "application/json"});
+
 twitterWorker(trackedWords);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
--- twitter.js
+++ twitter.js
@@ -3,9 +3,7 @@ var worker = function (words) {
     var redis = require("redis");
     var credentials = require("./credentials.js");                                                                                                                                                                                                                     
     var client = redis.createClient();
-    //console.log(words);
-    
-    
+
     var t = new twitter({
         consumer_key: credentials.consumer_key,
         consumer_secret: credentials.consumer_secret,
@@ -15,30 +13,16 @@ var worker = function (words) {
     
     t.stream(
         "statuses/filter",
-        { track: words }, // 'awesome', 'cool', 'rad', 'gnarly', 'groovy'] },
+        { track: words },
         function(stream) {
             stream.on('data', function(tweet) {
-                console.log(tweet.text);
+                //console.log(tweet.text);
                 words.forEach(function(word) {
                     if (tweet.text.indexOf(word) > -1) { 
                         client.incr(word);
-                        console.log("indexOF");
-                        console.log(word); 
-                    }
-                    if (tweet.text.match(word)) { 
-                        client.incr(word);
-                        console.log("match");
-                        console.log(word);
                     }
                 });
             });
-                /*if awesome is in the tweet text, increment the counter                                                                                                                                                                        
-                if(tweet.text.match(/shouldnt/)) { //if (tweet.text.indexOf("awesome") > -1)
-                    client.incr('shouldnt');
-                }
-                if(tweet.text.indexOf("cant") > -1) { //regular expression .match may be better
-                  client.incr("cant");
-                } */
         }
     );
 };
